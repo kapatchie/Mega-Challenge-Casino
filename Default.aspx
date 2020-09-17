94a32a77ac327847a1c43b4c56078865c65015ba
kapatchie <underdog.dog5@gmail.com>
2020-09-17 08:46:20 +0200

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Mega_Challenge_Casino.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="leftImageReel" runat="server" Height="150px" Width="150px" />
+            <asp:Image ID="middleImageReel" runat="server" Height="150px" Width="150px" />
+            <asp:Image ID="rightImageReel" runat="server" Height="150px" Width="150px" />
+            <br />
+            <br />
+            Your Bet: <asp:TextBox ID="betTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="leverButton" runat="server" OnClick="leverButton_Click" Text="Pull The  Lever" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Player&#39;s Money <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            1 Cherry - 2X Your Bet<br />
+            2 Cherries - 3X Your Bet<br />
+            3 Cherries - 4X Your Bet<br />
+            <br />
+            3 7&#39;s - JackPot - 100X Your Bet<br />
+            <br />
+            Any Bar and you Loose</div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mega_Challenge_Casino
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random random = new Random();
+        int playerMoney;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                playerMoney = 100;
+                moneyLabel.Text = playerMoney.ToString();
+                SelectImages(generateRandom());
+            }
+        }
+
+        public void leverButton_Click(object sender, EventArgs e)
+        {
+            int[] randomNumber = generateRandom();
+            playerMoney = int.Parse(moneyLabel.Text);
+            if (playerMoney > 0)
+            {
+                if (checkIfAnyInput())
+                {
+                    int betAmount = checkInput(betTextBox.Text);
+                    SelectImages(randomNumber);
+                    if (CheckMatches(randomNumber)[0] > 0 || CheckMatches(randomNumber)[1] == 3)
+                    {
+                        int winnings = calculateResults((betAmount), CheckMatches(randomNumber));
+                        resultLabel.Text = string.Format("You Won {0:C}", winnings);
+                        playerMoney += winnings;
+                        moneyLabel.Text = playerMoney.ToString();
+                    }
+                    else
+                    {
+                        resultLabel.Text = string.Format("You Lost {0:C}", betAmount);
+                        playerMoney -= betAmount;
+                        moneyLabel.Text = playerMoney.ToString();
+                    }
+                }
+            }
+        }
+
+        private int[] generateRandom()
+        {
+            int[] randomNumbers = new int[] { random.Next(0, 11), random.Next(0, 11), random.Next(0, 11) };
+            return randomNumbers;
+        }
+        private void SelectImages(int[] randomNumbers)
+        {
+                string[] imagesUrl = new string[] { @"~/Images/Bar.png",@"~/Images/Cherry.png",@"~/Images/Seven.png", @"~/Images/Bell.png",  
+                @"~/Images/Clover.png", @"~/Images/Diamond.png", @"~/Images/HorseShoe.png", @"~/Images/Lemon.png", 
+                @"~/Images/Orange.png", @"~/Images/Plum.png",  @"~/Images/Strawberry.png", 
+                @"~/Images/Watermellon.png"};
+
+            leftImageReel.ImageUrl = imagesUrl[randomNumbers[0]];
+            middleImageReel.ImageUrl = imagesUrl[randomNumbers[1]];
+            rightImageReel.ImageUrl = imagesUrl[randomNumbers[2]];
+        }
+        private bool checkIfAnyInput()
+        {
+            if (betTextBox.Text.Trim().Length > 0) return true;
+
+            return false;
+        }
+        private int checkInput(string betAmount)
+        {
+            if (!int.TryParse(betTextBox.Text, out int betAmountValue)) return 0; ;
+            return betAmountValue;
+        }
+        private int[] CheckMatches(int[] randomNumbers)
+        {
+            int cherryCount = 0; int sevenCount = 0;
+
+            if (!randomNumbers.Contains(0))
+            {
+                for (int i = 0; i < randomNumbers.Length; i++)
+                {
+                    if (randomNumbers[i] == 1) cherryCount += 1;
+                    if (randomNumbers[i] == 2) sevenCount += 1;
+                }           
+            }
+            return new int[] {cherryCount,sevenCount};
+        } 
+       
+        private int calculateResults(int betAmount, int[] matches)
+        {         
+            if (matches[1] == 3) betAmount *= 100;
+            if (matches[0] == 1) betAmount *= 2;
+            if (matches[0] == 2) betAmount *= 3;
+            if (matches[0] == 3) betAmount *= 4;
+
+            return betAmount;
+        }
+    }
+}
